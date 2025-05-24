689c6324cbefb742e854d47bf00503ace51eed50
Khallad-خلاد <100222507+kh2x1@users.noreply.github.com>
2025-05-24 23:58:11 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let bd = document.querySelector("body");
+
 let div = document.querySelector(".line");
 let topLine = document.getElementById("top");
 let middle = document.getElementById("middle");
@@ -7,6 +9,12 @@ let header = document.querySelector("header");
 let nav = document.querySelector("nav");
 let ul = document.querySelector("ul");
 let logo = document.querySelector(".logo");
+
+let skills = document.getElementById("Skills");
+let experience = document.getElementById("Experience");
+let education = document.getElementById("Education");
+let projects = document.getElementById("Projects");
+
 
 
 div.addEventListener("click", () => {
@@ -29,6 +37,97 @@ div.addEventListener("click", () => {
     ul.style.transition = "all 0.3s ease-in-out";
     header.style.transition = "all 0.3s ease-in-out";
     nav.style.display = "none";
+  }
+});
+
+
+window.addEventListener("scroll", () => {
+  
+  if (window.scrollY ==  300) {
+    skills.style.visibility = "visible";
+    skills.classList.add("skill");
+
+    
+
+    let scrollHeight = document.documentElement.scrollHeight - window.innerHeight;
+    let scrollPosition = window.scrollY;
+    if (scrollPosition == 300) {
+      console.log("hhh");
+    }
+  }
+
+  if (window.scrollY < 200) {
+    skills.style.visibility = "hidden";
+    skills.classList.remove("skill");
+  }
+});
+
+
+
+
+window.addEventListener("scroll", () => {
+  
+  if (window.scrollY ==  600) {
+    experience.style.visibility = "visible";
+    experience.classList.add("skill");
+
+    
+
+    let scrollHeight = document.documentElement.scrollHeight - window.innerHeight;
+    let scrollPosition = window.scrollY;
+    if (scrollPosition == 300) {
+      console.log("hhh");
+    }
+  }
+
+  if (window.scrollY < 300) {
+    experience.style.visibility = "hidden";
+    experience.classList.remove("skill");
+  }
+});
+
+
+
+
+window.addEventListener("scroll", () => {
+  
+  if (window.scrollY ==  900) {
+    education.style.visibility = "visible";
+    education.classList.add("skill");
+
+    
+
+    let scrollHeight = document.documentElement.scrollHeight - window.innerHeight;
+    let scrollPosition = window.scrollY;
+    if (scrollPosition == 300) {
+      console.log("hhh");
+    }
+  }
+
+  if (window.scrollY < 600) {
+    education.style.visibility = "hidden";
+    education.classList.remove("skill");
+  }
+});
+
+
+
+
+window.addEventListener("scroll", () => {
+  
+  if (window.scrollY ==  1200) {
+    projects.style.visibility = "visible";
+    projects.classList.add("skill");
+    // let scrollHeight = document.documentElement.scrollHeight - window.innerHeight;
+    // let scrollPosition = window.scrollY;
+    // if (scrollPosition == 300) {
+    //   console.log("hhh");
+    // }
+  }
+
+  if (window.scrollY < 900) {
+    projects.style.visibility = "hidden";
+    projects.classList.remove("skill");
   }
 });
 
